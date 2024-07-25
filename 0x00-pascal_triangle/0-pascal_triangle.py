e1513803620da8b509bf711446fa1708284246b3
easydigs <easydigitalkoncept@gmail.com>
2024-07-25 19:33:39 +0100

    Task 0 updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,11 +6,13 @@ Args:
     n (int): The number of rows in Pascal's triangle.
 
 Returns:
-    list of lists: Pascal's triangle up to the nth row. 
+    list of lists: Pascal's triangle up to the nth row.
     Returns an empty list if n <= 0.
 """
 
+
 def pascal_triangle(n):
+
     """
     Generate Pascal's triangle up to the nth row.
 
@@ -18,7 +20,7 @@ def pascal_triangle(n):
         n (int): The number of rows in Pascal's triangle.
 
     Returns:
-        list of lists: Pascal's triangle up to the nth row. 
+        list of lists: Pascal's triangle up to the nth row.
         Returns an empty list if n <= 0.
     """
     if n <= 0:
@@ -30,7 +32,8 @@ def pascal_triangle(n):
         row = [1]  # Start each row with a 1
 
         for j in range(1, i):
-            # The value is the sum of the two values above it in the previous row
+            # The value is the sum of the two values above
+            # it in the previous row
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
 
         row.append(1)  # End each row with a 1
