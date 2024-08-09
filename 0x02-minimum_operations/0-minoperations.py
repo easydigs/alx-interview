2dd85f2a0e23b836a4a78ad712f27ca7b0d2a436
easydigs <easydigitalkoncept@gmail.com>
2024-08-09 05:36:28 +0100

    Task 0 Min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,18 +6,24 @@ Returns: Minimum number of operations needed to get exactly n 'H' characters
 
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
+    """ Minimum number of operations needed to get exactly n 'H' characters """
+    next_segment = 'H'
     body = 'H'
     op = 0
-    while (len(body) < n):
+
+    for i in range(1, n):
         if n % len(body) == 0:
-            op += 2
-            next = body
+            op += 2  # Copy All + Paste
+            next_segment = body
             body += body
         else:
-            op += 1
-            body += next
+            op += 1  # Paste only
+            body += next_segment
+
+        if len(body) == n:
+            break
+
     if len(body) != n:
         return 0
+
     return op
